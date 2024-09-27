1da9ad55332005d3a8462def8627c5189bcb6fea
Benny Dabush <bennydabush@gmail.com>
2024-09-27 11:10:48 +0300

    adding loading to the dashboard

--- src/dashboard/pages/DashboardPage.jsx
+++ src/dashboard/pages/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, CircularProgress, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import DataTable from "../components/DataTable";
 import LoginToDashboard from "./LoginToDashboard";
@@ -8,6 +8,8 @@ import adminUser from "../../lib/adminUser";
 
 export default function DashboardPage() {
   const [isLogged, setIsLogged] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
   const [data, setData] = useState([]);
   const [started, setStarted] = useState(0);
   const [stage1, setStage1] = useState(0);
@@ -20,12 +22,16 @@ export default function DashboardPage() {
       setStarted(allUsers.length);
       setStage1(allUsers.filter((user) => user.stage >= 2).length);
       setStage2(allUsers.filter((user) => user.stage === 3).length);
+      setIsLoading(false);
     };
     getAllData();
   }, []);
 
   if (!isLogged) {
     return <LoginToDashboard setIsLogged={setIsLogged} />;
+  }
+  if (isLoading) {
+    return <CircularProgress />;
   }
   return (
     <Box sx={{ direction: "rtl" }}>
